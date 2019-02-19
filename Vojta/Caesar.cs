a1092578b1c28cec5f69973388c88dbbab8b6ef7
Vojta Karasek <vojta.karasek1@gmail.com>
2019-02-19 19:46:28 +0100

    caesar cipher

---
+++ Vojta/Caesar.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vojta
+{
+    public class Caesar
+    {
+        const int AlphabetSize = 'z' - 'a' + 1;
+
+        public string PrepareData(string input)
+        {
+            var sb = new StringBuilder();
+            foreach (var ch in input)
+            {
+                if (ch >= 'a' && ch <= 'z')
+                    sb.Append(ch);
+                else if (ch >= 'A' && ch <= 'Z')
+                    sb.Append(char.ToLower(ch));
+            }
+            return sb.ToString();
+        }
+
+        public string Encode(string plain, int key )
+        {
+            var sb = new StringBuilder();
+            foreach(var ch in plain)
+            {
+                var ascii = ch + key;
+                if (ascii > 'z')
+                    ascii -= AlphabetSize;
+                if (ascii < 'a')
+                    ascii += AlphabetSize;
+                var c = char.ToUpper((char)ascii);
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+
+        public string Decode(string cipher, int key)
+        {
+            var cipherLow = cipher.ToLower();
+            return Encode(cipherLow, -key).ToLower();
+        }
+
+    }
+}
---
+++ VojtaTest/CaesarTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Vojta;
+
+namespace VojtaTest
+{
+    public class CaesarTest
+    {
+        [Theory]
+        [InlineData("Ahoj Fiku, kupuj Umtitiho!", "ahojfikukupujumtitiho")]
+        void PreparesData(string input, string output)
+        {
+            var caesar = new Caesar();
+            Assert.Equal(output, caesar.PrepareData(input));
+        }
+
+        [Theory]
+        [InlineData("abc", 3, "DEF")]
+        [InlineData("xyz", 3, "ABC")]
+        [InlineData("ahojfikukupujumtitiho", 3, "DKRMILNXNXSXMXPWLWLKR")]
+        void Encodes(string plain, int key, string cipher)
+        {
+            var caesar = new Caesar();
+            Assert.Equal(cipher, caesar.Encode(plain, key));
+        }
+
+        [Theory]
+        [InlineData("ABC", 3, "xyz")]
+        [InlineData("DKRMILNXNXSXMXPWLWLKR", 3, "ahojfikukupujumtitiho")]
+        void Decodes(string cipher, int key, string plain)
+        {
+            var caesar = new Caesar();
+            Assert.Equal(plain, caesar.Decode(cipher, key));
+        }
+
+    }
+}
