61ba5853141ff1d098b925fbb826b2c6c0850679
Vojta Karasek <vojta.karasek1@gmail.com>
2019-03-17 19:17:42 +0100

    BubbleSort

---
+++ Vojta/Sorter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vojta
+{
+    public class Sorter
+    {
+        public void BubbleSort(int[] numbers)
+        {
+            for (var i = 0; i < numbers.Length; i++)
+                for (var j = 0; j < numbers.Length - 1; j++)
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        var storage = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = storage;
+                    }
+        }
+    }
+}
---
+++ VojtaTest/SortTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using Vojta;
+
+namespace VojtaTest
+{
+    public class SortTest
+    {
+        [Fact]
+        public void BubbleSortWorks()
+        {
+            var sorter = new Sorter();
+            var numbers = new int[] { 6, 3, 2 };
+            sorter.BubbleSort(numbers);
+            Assert.Equal(new[] { 2, 3, 6 }, numbers);
+        }
+    }
+}
