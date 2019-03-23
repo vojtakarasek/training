efe240ba3a0f702d72c18e8e1681fe10cf647b3d
Vojta Karasek <vojta.karasek1@gmail.com>
2019-03-23 20:04:58 +0100

    QuickSort

--- Vojta/Sorter.cs
+++ Vojta/Sorter.cs
@@ -17,5 +17,45 @@ namespace Vojta
                         numbers[j + 1] = storage;
                     }
         }
+
+        public void QuickSort(int[] numbers)
+        {
+            QuickSort(numbers, 0, numbers.Length);
+        }
+
+        void QuickSort(int[] numbers, int start, int end)
+        {
+            if (start == end) return;
+            var middle = Partition(numbers, start, end);
+            QuickSort(numbers, start, middle);
+            QuickSort(numbers, middle + 1, end);
+        }
+
+        int Partition(int[] numbers, int start, int end)
+        {
+            var pivot = numbers[start];
+            var l = start + 1;
+            var r = end - 1;
+            while (true)
+            {
+                while (l <= r && numbers[l] < pivot)
+                    l++;
+                while (numbers[r] > pivot && r >= l)
+                    r--;
+                if (l > r)
+                    break;
+                else
+                    Swap(numbers, l, r);
+            }
+            Swap(numbers, start, r);
+            return r;
+        }
+
+        void Swap(int[] numbers, int l, int r)
+        {
+            var storage = numbers[r];
+            numbers[r] = numbers[l];
+            numbers[l] = storage;
+        }
     }
 }
--- VojtaTest/SortTest.cs
+++ VojtaTest/SortTest.cs
@@ -13,5 +13,14 @@ namespace VojtaTest
             sorter.BubbleSort(numbers);
             Assert.Equal(new[] { 2, 3, 6 }, numbers);
         }
+
+        [Fact]
+        public void QuickSortWorks()
+        {
+            var sorter = new Sorter();
+            var numbers = new int[] { 6, 3, 2 };
+            sorter.QuickSort(numbers);
+            Assert.Equal(new[] { 2, 3, 6 }, numbers);
+        }
     }
 }
