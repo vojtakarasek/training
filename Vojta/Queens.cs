5e6919d4910773248c0b1d2b45c656e833d28bbd
Vojta Karasek <vojta.karasek1@gmail.com>
2019-04-02 21:42:50 +0200

    WIP: n-queen problem

---
+++ Vojta/Queens.cs
@@ -0,0 +1,61 @@
+﻿namespace Vojta
+{
+    public struct Position
+    {
+        public int Column;
+        public int Row;
+    }
+
+    public class Board
+    {
+        bool[] _attackedColumns;
+        bool[] _attackedRows;
+        bool[] _attackedDiagU;
+        bool[] _attackedDiagD;
+        int _size;
+
+        public Board(int size)
+        {
+            _size = size;
+            _attackedColumns = new bool[size];
+            _attackedRows = new bool[size];
+            _attackedDiagD = new bool[2 * size - 1];
+            _attackedDiagU = new bool[2 * size - 1];
+        }
+
+        public bool TryPlaceQueen(Position pos)
+        {
+            if (_attackedColumns[pos.Column]) return false;
+            if (_attackedRows[pos.Row]) return false;
+            if (_attackedDiagU[GetDiagU(pos)]) return false;
+            if (_attackedDiagD[GetDiagD(pos)]) return false;
+
+            _attackedColumns[pos.Column] = true;
+            _attackedRows[pos.Row] = true;
+            _attackedDiagU[GetDiagU(pos)] = true;
+            _attackedDiagD[GetDiagD(pos)] = true;
+
+            return true;
+        }
+
+        int GetDiagU(Position pos)
+        {
+            return pos.Column + (_size - pos.Row);
+        }
+
+        int GetDiagD(Position pos)
+        {
+            return pos.Column + pos.Row;
+        }
+    }
+
+
+
+    public class QueenSolver
+    {
+        public QueenSolver(int boardSize)
+        {
+            
+        }
+    }
+}
---
+++ VojtaTest/QueensTest.cs
@@ -0,0 +1,33 @@
+﻿using Vojta;
+using Xunit;
+
+namespace VojtaTest
+{
+    public class QueensTest
+    {
+        [Fact]
+        public void BoardChecksPosition()
+        {
+            var board = new Board(4);
+            var corner = new Position { Column = 0, Row = 0 };
+            Assert.True(board.TryPlaceQueen(corner));
+            Assert.False(board.TryPlaceQueen(new Position { Column = 1, Row = 0 }));
+            Assert.False(board.TryPlaceQueen(new Position { Column = 1, Row = 1 }));
+            Assert.False(board.TryPlaceQueen(new Position { Column = 0, Row = 1 }));
+            Assert.False(board.TryPlaceQueen(new Position { Column = 3, Row = 3 }));
+            Assert.True(board.TryPlaceQueen(new Position { Column = 1, Row = 2 }));
+            Assert.True(board.TryPlaceQueen(new Position { Column = 3, Row = 1 }));
+        }
+
+
+
+
+
+
+
+
+    }
+    
+
+
+}
