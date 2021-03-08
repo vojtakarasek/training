e45c09f467d8389182ffeb59d1b35a83bb463fbe
Vojta <vojta.karasek1@gmail.com>
2021-03-08 20:06:55 +0100

    Factorial practice

--- Vojta/Factorial.cs
+++ Vojta/Factorial.cs
@@ -1,13 +1,38 @@
-﻿namespace Vojta
+﻿using System.Linq;
+
+namespace Vojta
 {
     public class FactorialCalc
     {
-        //F(0)=1, F(1)=1, F(N)=F(N) * F(N-1) * .. 1
+        //F(0)=1, F(1)=1, F(N)= N * F(N-1)
         public int Factorial(int n)
         {
             if (n == 0)
                 return 1;
             return n * Factorial(n - 1);
+        }
+
+        public int FactorialCycle(int n)
+        {
+            var result = 1;
+            while (n > 0)
+            {
+                result = n * result;
+                n--;
+            }
+            return result;
+        }
+
+        public int FactorialRange(int n) => Enumerable.Range(1, n).Aggregate(1, (acc, i) => acc * i);
+
+        public int FactorialFor(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
         }
     }
 }
--- VojtaTest/FactorialTest.cs
+++ VojtaTest/FactorialTest.cs
@@ -16,5 +16,19 @@ namespace VojtaTest
             var f = new FactorialCalc();
             Assert.Equal(result, f.Factorial(n));
         }
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 6)]
+        [InlineData(5, 120)]
+        void FactVariants(int n, int result)
+        {
+            var f = new FactorialCalc();
+            Assert.Equal(result, f.FactorialCycle(n));
+            Assert.Equal(result, f.FactorialRange(n));
+            Assert.Equal(result, f.FactorialFor(n));
+        }
     }
 }
