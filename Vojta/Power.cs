8bea6630c6692c215077d5a6503e572175e59435
Vojta <vojta.karasek1@gmail.com>
2021-03-20 16:15:12 +0100

    :)

---
+++ Vojta/Power.cs
@@ -0,0 +1,15 @@
+﻿namespace Vojta
+{
+    public class Power
+    {
+        public double Pow(double x, int n)
+        {
+            if (n == 0)
+                return 1;
+            var t = Pow(x, n / 2);
+            if (n % 2 == 0)
+                return t * t;
+            return t * t * x;
+        }
+    }
+}
---
+++ VojtaTest/PowTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using Vojta;
+
+namespace VojtaTest
+{
+    public class PowTest
+    {
+        [Fact]
+        public void BubbleSortWorks()
+        {
+            var pow = new Power();
+            Assert.Equal(System.Math.Pow(8, 12), pow.Pow(8, 12));
+        }
+    }
+}
