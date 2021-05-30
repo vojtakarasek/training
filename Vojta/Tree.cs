1c9548993f26e069b46d7b28ad4160694ebd41ac
Vojta <vojta.karasek1@gmail.com>
2021-05-30 18:08:35 +0200

    trees

---
+++ Vojta/Tree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vojta
+{
+    public class Tree
+    {
+        public int Value { get; set; }
+        public Tree Left { get; }
+        public Tree Right { get; }
+
+        public Tree(int value, Tree left = null, Tree right = null)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+
+        public string Iterate(string accumulator, Func<string, int, string> aggr)
+        {
+            if (Left != null)
+                 accumulator = Left.Iterate(accumulator, aggr);
+            accumulator = aggr(accumulator, Value);
+            if (Right != null)
+                accumulator = Right.Iterate(accumulator, aggr);
+            return accumulator;
+        }
+    }
+
+
+}
---
+++ VojtaTest/TreeTest.cs
@@ -0,0 +1,24 @@
+﻿using Vojta;
+using Xunit;
+
+namespace VojtaTest
+{
+    public class TreeTest
+    {
+        [Fact]
+        void TreeIteration()
+        {
+            var tree = new Tree(5,
+                left: new Tree(2, left: new Tree(1)),
+                right: new Tree(7, left: new Tree(6), right: new Tree(8)));
+
+            var list = tree.Iterate("", (acc, value) =>
+                {
+                    if (acc == "") return value.ToString();
+                    return acc + ", " + value.ToString();
+                });
+
+            Assert.Equal("1, 2, 5, 6, 7, 8", list);
+        }
+    }
+}
